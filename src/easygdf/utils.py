17b5e0203c981a469774a19b9847c277b9c9edcb
Christopher M. Pierce <chris@chris-pierce.com>
2025-01-12 22:07:47 -0800

    update to `importlib.resources`

--- src/easygdf/utils.py
+++ src/easygdf/utils.py
@@ -1,27 +1,26 @@
 #  This file is part of easygdf and is released under the BSD 3-clause license
 
-import pkg_resources
+from importlib.resources import files
 
 
 def get_example_screen_tout_filename():
     """
-    Unpacks an example of a screen/tout GDF file from easygdf and returns the path.  Used for testing and running demos
-    of library.
+    Returns the path to an example screen/tout GDF file from easygdf.
+    Used for testing and running demos of library.
 
     :return: Path to the example file
     """
-    return pkg_resources.resource_filename("easygdf", "data/screens_touts.gdf")
+    return str(files("easygdf").joinpath("data/screens_touts.gdf"))
 
 
 def get_example_initial_distribution():
     """
-    Unpacks an example of an initial distribution GDF file from easygdf and returns the path.  Used for testing and
-    running demos of library.
+    Returns the path to an example initial distribution GDF file from easygdf.
+    Used for testing and running demos of library.
 
     :return: Path to the example file
     """
-    return pkg_resources.resource_filename("easygdf", "data/initial_distribution.gdf")
-
+    return str(files("easygdf").joinpath("data/initial_distribution.gdf"))
 
 class GDFError(Exception):
     pass
